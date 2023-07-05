38057d40157b1612d0798aa90c5b5410bd5d867e
chuijse <cristian.huijse@gmail.com>
2023-07-05 18:21:29 -0400

    responsive multi-platform

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import styles from "./page.module.scss";
+import styles from "./_page.module.scss";
 
 export default function Home() {
   return (
@@ -11,28 +11,34 @@ export default function Home() {
         </h2>
       </div>
       <div className={styles.title}>
-        <h1>Diseñador, Desarrollador y docente</h1>
+        <h1>Diseñador, Desarrollador y Docente</h1>
       </div>
       <div className={styles.imageItem}>
         <Image
           src="/static/images/perfil-website.jpg"
           fill
-          style={{ objectFit: "cover" }}
+          alt="Foto de perfil cristian Huijse heise"
+          style={{ objectFit: "cover", objectPosition: "center" }}
         />
       </div>
       <div className={styles.abstract}>
         <p>
           ¡Bienvenido/a a mi portafolio! Aquí encontrarás una amplia variedad de
-          disciplinas, pero mi enfoque principal se centra en el diseño gráfico
-          e industrial, así como en el desarrollo de software, especialmente en
-          el frontend. Te invito a visitar mis proyectos
+          disciplinas, pero mi enfoque
+          <wbr /> principal se centra en el diseño gráfico e industrial, así
+          como en el desarrollo de software, especialmente en el frontend. Te
+          invito a visitar mis proyectos
         </p>
         <div className={styles.button}>
           <a className={styles.buttonText} href="http://chuijse.github.io">
             Descargar Portafolio
           </a>
           <div className={styles.flecha}>
-            <Image fill src="/static/images/arrow-small.svg" />
+            <Image
+              fill
+              src="/static/images/arrow-small.svg"
+              alt="flecha de botton"
+            />
           </div>
         </div>
       </div>
