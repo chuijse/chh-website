16a1513fb26740d37df5e7487b63c863b570ba5e
chuijse <cristian.huijse@gmail.com>
2023-07-05 01:02:13 -0400

    abstract home

--- app/page.js
+++ app/page.js
@@ -1,95 +1,41 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.scss";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+      <div className={styles.iam}>
+        <h2>
+          Hola soy <br />
+          <b className={styles.name}>Cristian Huijse Heise</b>
+        </h2>
       </div>
-
-      <div className={styles.center}>
+      <div className={styles.title}>
+        <h1>Diseñador, Desarrollador y docente</h1>
+      </div>
+      <div className={styles.imageItem}>
         <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src="/static/images/perfil-website.jpg"
+          fill
+          style={{ objectFit: "cover" }}
         />
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className={styles.abstract}>
+        <p>
+          ¡Bienvenido/a a mi portafolio! Aquí encontrarás una amplia variedad de
+          disciplinas, pero mi enfoque principal se centra en el diseño gráfico
+          e industrial, así como en el desarrollo de software, especialmente en
+          el frontend. Te invito a visitar mis proyectos
+        </p>
+        <div className={styles.button}>
+          <a className={styles.buttonText} href="http://chuijse.github.io">
+            Descargar Portafolio
+          </a>
+          <div className={styles.flecha}>
+            <Image fill src="/static/images/arrow-small.svg" />
+          </div>
+        </div>
       </div>
     </main>
-  )
+  );
 }
