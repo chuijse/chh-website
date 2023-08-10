72e4be85c4597f54408c2df2929e013825f6f9c0
chuijse <cristian.huijse@gmail.com>
2023-08-10 00:01:00 -0400

    name, subjectm, mail and message css

--- app/components/contact/index.js
+++ app/components/contact/index.js
@@ -1,48 +1,78 @@
+"use client";
+
 import styles from "./_contact.module.scss";
+import { useRef, useState, useEffect } from "react";
 
 export default function Contact() {
+  const textRef = useRef();
+  const [value, setValue] = useState();
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  useEffect(() => {
+    if (textRef && textRef.current) {
+      textRef.current.style.height = "0px";
+      const taHeight = textRef.current.scrollHeight;
+      textRef.current.style.height = taHeight + "px";
+    }
+  }, [value]);
+
   return (
     <section className={styles.root}>
-      <div>
-        <h2>Contact</h2>
-        <p>Te invito a escribirme a través de este formulario</p>
-        <form>
-          <label>Nombre:</label>
-          <input
-            type="text"
-            id="form_name"
-            name="form_name"
-            required
-            placeholder="Escriba aquí"
-          />
-          <label>Asunto:</label>
-          <input
-            type="text"
-            id="form_subject"
-            name="form_subject"
-            required
-            placeholder="Escriba aquí"
-          />
-          <label>Mail:</label>
-          <input
-            type="email"
-            id="form_mail"
-            name="form_mail"
-            required
-            placeholder="Escriba aquí"
-          />
-          <label htmlFor="message">Mensaje:</label>
-          <textarea
-            type="text"
-            id="form_message"
-            name="form_message"
-            required
-            placeholder="Escriba aquí"
-          ></textarea>
-          <input type="submit" />
+      <div className={styles.header}>
+        <h3 className={styles.title}>Contacto</h3>
+        <p className={styles.description}>
+          Te invito a escribirme a través de este formulario
+        </p>
+
+        <form className={styles.form} autocomplete="off">
+          <div className={styles.input_half}>
+            <input
+              type="text"
+              id="form_name"
+              name="form_name"
+              required=""
+              placeholder="Escriba aquí*"
+            />
+            <label for="form_name">Nombre:</label>
+          </div>
+          <div className={styles.input_half}>
+            <input
+              type="text"
+              id="form_subject"
+              name="form_subject"
+              required=""
+              placeholder="Escriba aquí"
+            />
+            <label>Asunto:</label>
+          </div>
+          <div className={styles.input}>
+            <input
+              type="email"
+              id="form_mail"
+              name="form_mail"
+              required
+              placeholder="Escriba aquí"
+            />
+            <label>Mail:</label>
+          </div>
+          <div className={styles.input_textarea}>
+            <textarea
+              type="text"
+              id="form_message"
+              name="form_message"
+              required
+              placeholder="Escriba aquí"
+              onChange={handleChange}
+              ref={textRef}
+            />
+            <label htmlFor="message">Mensaje:</label>
+          </div>
+          <input type="submit" className={styles.input_button} />
         </form>
       </div>
-      <div></div>
     </section>
   );
 }
