e2585bbd6548beb419fb02a92db1d27fb0071850
chuijse <cristian.huijse@gmail.com>
2023-09-02 22:29:21 -0400

    change mailjs-com

--- app/components/contact/ContactFoms.js
+++ app/components/contact/ContactFoms.js
@@ -1,9 +1,12 @@
 "use client";
 import { useRef, useState, useEffect } from "react";
+import emailjs from "emailjs-com";
 
 export default function ContactForm() {
   const textRef = useRef();
+  const form = useRef();
   const [value, setValue] = useState();
+  const [buttonMessage, setButtonMassege] = useState("Enviar");
 
   const handleChange = (event) => {
     setValue(event.target.value);
@@ -17,33 +20,52 @@ export default function ContactForm() {
     }
   }, [value]);
 
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
+        process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
+        form.current,
+        process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setButtonMassege("enviado");
+        },
+        (error) => {
+          console.log(error.text);
+          alert("Error sending message, try again later");
+        }
+      );
+  };
+
   return (
-    <form className="contact-form" autoComplete="off">
+    <form
+      className="contact-form"
+      autoComplete="off"
+      ref={form}
+      onSubmit={sendEmail}
+    >
       <div className="input_half">
-        <input
-          type="text"
-          id="form_name"
-          name="form_name"
-          required
-          placeholder="Escriba aquí*"
-        />
-        <label htmlFor="form_name">Nombre:</label>
+        <input name="user_name" required placeholder="Escriba aquí*" />
+        <label htmlFor="user_name">Nombre:</label>
       </div>
       <div className="input">
         <input
           type="email"
-          id="form_mail"
-          name="form_mail"
+          name="user_mail"
           required
           placeholder="Escriba aquí"
         />
-        <label htmlFor="form_mail">Mail:</label>
+        <label htmlFor="user_mail">Mail:</label>
       </div>
       <div className="input_textarea">
         <textarea
           type="text"
-          id="form_message"
-          name="form_message"
+          name="message"
           required
           placeholder="Escriba aquí"
           onChange={handleChange}
@@ -51,8 +73,8 @@ export default function ContactForm() {
         />
         <label htmlFor="message">Mensaje:</label>
       </div>
-      <button type="submit" className="input_button" value="enviar">
-        Enviar
+      <button type="submit" className="input_button">
+        {buttonMessage}
       </button>
     </form>
   );
