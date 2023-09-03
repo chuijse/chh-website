8baed0b77cb0d1cb865ebae8ac509cdf50f09271
chuijse <cristian.huijse@gmail.com>
2023-09-03 20:54:12 -0300

    add nodeMailer

--- app/components/contact/ContactFoms.js
+++ app/components/contact/ContactFoms.js
@@ -1,40 +1,32 @@
 "use client";
 import { useRef, useState, useEffect } from "react";
 import emailjs from "emailjs-com";
+import { useForm } from "react-hook-form";
+import { sendEmail } from "../../utils/send-email";
 
-const initValues = { name: "", email: "", massage: "" };
+const initValues = { name: "", email: "", message: "" };
 
 export default function ContactForm() {
-  const [state, setSatet] = useState(initValues);
+  const [state, setState] = useState(initValues);
   const textRef = useRef();
   const form = useRef();
-  const [value, setValue] = useState();
+  const [values, setValues] = useState();
   const [buttonMessage, setButtonMassege] = useState("Enviar");
+  const [name, setName] = useState();
 
-  /*const handleChange = (event) => {
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    //setValue(event.target.value);
+  };
+
+  console.log(state);
+
+  /*
+  const handleChange = (event) => {
     setValue(event.target.value);
   };*/
 
-  /*const values = state;
-  console.log(values);
-
-  const handleChange = ({ target }) => {
-    setValue((prev) => ({
-      ...prev,
-      values: {
-        ...prev.values,
-        [target.name]: target.value,
-      },
-    }));
-    console.log(target.value);
-  };
-
-  */
-
-  const handleChange = (event) => {
-    setValue(event.target.value);
-  };
-
+  /*
   useEffect(() => {
     if (textRef && textRef.current) {
       textRef.current.style.height = "0px";
@@ -64,21 +56,26 @@ export default function ContactForm() {
         }
       );
   };
+  */
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    sendEmail(state);
+  }
 
   return (
     <form
       className="contact-form"
       autoComplete="off"
       ref={form}
-      onSubmit={sendEmail}
+      onSubmit={onSubmit}
     >
       <div className="input_half">
         <input
           name="user_name"
           required
           placeholder="Escriba aquí*"
-          //value={values.name}
-          //onChange={handleChange}
+          onChange={(e) => setState({ ...state, name: e.target.value })}
         />
         <label htmlFor="user_name">Nombre:</label>
       </div>
@@ -88,6 +85,7 @@ export default function ContactForm() {
           name="user_mail"
           required
           placeholder="Escriba aquí"
+          onChange={(e) => setState({ ...state, email: e.target.value })}
         />
         <label htmlFor="user_mail">Mail:</label>
       </div>
@@ -97,14 +95,12 @@ export default function ContactForm() {
           name="message"
           required
           placeholder="Escriba aquí"
-          onChange={handleChange}
+          onChange={(e) => setState({ ...state, message: e.target.value })}
           ref={textRef}
         />
         <label htmlFor="message">Mensaje:</label>
       </div>
-      <button type="submit" className="input_button">
-        {buttonMessage}
-      </button>
+      <input type="submit" className="input_button"></input>
     </form>
   );
 }
---
+++ app/utils/send-email.js
@@ -0,0 +1,16 @@
+export function sendEmail(data) {
+  const apiEndpoint = "/api/email";
+  console.log(JSON.stringify(data));
+
+  fetch(apiEndpoint, {
+    method: "POST",
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .then((response) => {
+      alert(response.message);
+    })
+    .catch((err) => {
+      alert(err);
+    });
+}
